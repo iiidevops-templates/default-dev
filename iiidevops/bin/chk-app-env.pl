1f89bc63a0b0de42e20a14a12200bafa50fa81aa
jonathan <tryweb@ichiayi.com>
2021-10-24 08:45:08 +0800

    改用 croak() 取代 die()

--- iiidevops/bin/chk-app-env.pl
+++ iiidevops/bin/chk-app-env.pl
@@ -1,4 +1,6 @@
 #!/usr/bin/perl
+use Carp;
+
 $branch = defined($ARGV[0])?lc($ARGV[0]):'master';
 $env_file = 'iiidevops/app.env';
 $branch_env_file = "$env_file.$branch";
@@ -6,13 +8,13 @@ print("Check branch env_file : [$branch_env_file]..\n");
 if (-e $branch_env_file) {
 	#$tmpl = `cat $branch_env_file`;
 	$tmpl = '';
-	open(FH, '<', $branch_env_file) or die $!;
+	open(FH, '<', $branch_env_file) or croak "error opening $branch_env_file: stopped";
 	while(<FH>){
 		$tmpl = $_;
 	}
 	close(FH);
 	
-	open(FH, '>', $env_file) or die $!;
+	open(FH, '>', $env_file) or croak "error opening $env_file: stopped";
 	print FH $tmpl;
 	close(FH);
 	
