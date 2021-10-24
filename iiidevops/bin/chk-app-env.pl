86b4cd8b60ee9209fd5f016979579454a88577e3
jonathan <tryweb@ichiayi.com>
2021-10-24 15:02:27 +0800

    修正語法與增加 options 參數

--- iiidevops/bin/chk-app-env.pl
+++ iiidevops/bin/chk-app-env.pl
@@ -2,22 +2,29 @@
 use Carp;
 
 $branch = defined($ARGV[0])?lc($ARGV[0]):'master';
+$options = defined($ARGV[1])?lc($ARGV[1]):'none';
 $env_file = 'iiidevops/app.env';
 $branch_env_file = "$env_file.$branch";
-print("Check branch env_file : [$branch_env_file]..\n");
 if (-e $branch_env_file) {
-	#$tmpl = `cat $branch_env_file`;
+	print("Useing env_file : [$branch_env_file]..\n");
+
+	# replace branch_env_file to env_file
 	$tmpl = '';
 	open(FH, '<', $branch_env_file) or croak "error opening $branch_env_file: stopped";
 	while(<FH>){
-		$tmpl = $_;
+		$tmpl .= $_;
 	}
 	close(FH);
 	
 	open(FH, '>', $env_file) or croak "error opening $env_file: stopped";
 	print FH $tmpl;
-	close(FH);
-	
+	close(FH);	
+}
+else {
+	print("Useing env_file : [$env_file]..\n");
+}
+
+if ($options eq 'print') {
 	$cmd_msg = `cat $env_file`;
 	print("\n-----\n$cmd_msg\n-----\n");
 }
