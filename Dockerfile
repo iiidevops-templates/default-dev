a72cf01167346728b469b43be8fa854a9eaad800
jonathan <tryweb@ichiayi.com>
2022-01-26 10:07:35 +0800

    Test

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ FROM dockerhub/library/node:alpine
 COPY app /usr/app
 WORKDIR /usr/app
 RUN npm install
-EXPOSE 3000
+EXPOSE 8080
 CMD node sample.js
--- app/sample.js
+++ app/sample.js
@@ -1,11 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var http = require('http');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
